ea19e4e4ce8f04225bf3c3acc9828e9ac3640e0e
AgnesaS <44116298+AgnesaS@users.noreply.github.com>
2021-11-23 07:29:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,20 +6,32 @@
 
 
 // - Prompt the user for a letter
-
 let firstLetter = prompt(`Write a letter ->`)
 
+// - Find all the users who's name contains the letter 
 const userName= users.map( function(user){
     return user.name
 } )
 function firstNameLetter (){
     return userName.includes(firstLetter)
 }
-
+// - Say hi to those users in the console - but only 1 greeting every 2 seconds.
 for(const name of userName){
     console.log(`Hello ${name}`)
 }
 
-let idUsers = Number(prompt(`Write your ID ->`))
 
+// - Prompt the user for a user ID. Using the value, find all the todos that are Inc
+
+let idUsers = Number(prompt(`Write your ID ->`))
+const userIdMapped = todos.map(function(todo){
+    return todo.userId === idUsers
+})
+console.log(`Hello  ${idUsers}`)
+function idOfUser(){
+    return userIdMapped.includes(idUsers)
+
+}
 firstNameLetter()
+idOfUser()
+
