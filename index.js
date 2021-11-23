78a59243766b9d8472245b7aed4d8f284f1374ac
AgnesaS <44116298+AgnesaS@users.noreply.github.com>
2021-11-23 07:18:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+//With the usual Users and Todo lists:
+
+// - Find all the users who's name contains the letter 
+// - Say hi to those users in the console - but only 1 greeting every 2 seconds.
+// - Prompt the user for a user ID. Using the value, find all the todos that are Inc
+
+
+// - Prompt the user for a letter
+
 let firstLetter = prompt(`Write a letter ->`)
 
 const userName= users.map( function(user){
